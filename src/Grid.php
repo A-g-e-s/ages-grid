ecd42144b5d485665f15d6de260d108b9121c8ad
Ages <jiri.hovad@outlook.com>
2026-02-23 10:08:07 +0100

    Export name

--- src/Grid.php
+++ src/Grid.php
@@ -115,7 +115,7 @@ final class Grid extends UI\Control
         $e = new Export(
             $this->collection,
             $this->sortData(),
-            $this->caption,
+            $this->caption, //Return to caption
             basePath: $basePath,
             exportPath: $exportPath
         );
